a7f72cf4a5f2bf2deca847866c4db034ad697de8
stepanie1996 <130530497+stepanie1996@users.noreply.github.com>
2024-12-25 11:16:27 +0800

    Update NavBar.tsx

--- src/layouts/RootLayout/Header/NavBar.tsx
+++ src/layouts/RootLayout/Header/NavBar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 
 const NavBar: React.FC = () => {
-  const links = [{ id: 1, name: "更多", to: "/about" }]
+  const links = [{ id: 1, name: "下载说明", to: "/about" }]
   return (
     <StyledWrapper className="">
       <ul>
