939399f7009fb7fa7d79fb97e8b3bebc1bea2f73
zwz199 <135852339+zwz199@users.noreply.github.com>
2025-01-26 10:52:40 +0800

    Update getPosts.ts

--- src/apis/notion-client/getPosts.ts
+++ src/apis/notion-client/getPosts.ts
@@ -78,12 +78,7 @@ export const getPosts = async () => {
       data.push(properties)
     }
 
-    // Sort by date
-    data.sort((a: any, b: any) => {
-      const dateA: any = new Date(a?.date?.start_date || a.createdTime)
-      const dateB: any = new Date(b?.date?.start_date || b.createdTime)
-      return dateB - dateA
-    })
+
 
     const posts = data as TPosts
     return posts
